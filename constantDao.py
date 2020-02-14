bd4386c0bcf1d7e4e9971052f0981b910c1e9119
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-02-14 20:22:26 +0100

    Update constant DAO

--- constantDao.py
+++ constantDao.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 class ConstantDao:
@@ -6,27 +7,81 @@ class ConstantDao:
 
     def __init__(self):
         try:
-            self.cursor = self.sqliteConnection.cursor
-            self.initializeDatabase()
+            self.cursor = self.sqliteConnection.cursor()
+            # self.initializeDatabase()
+            self.getConstant("VIEW_ID")
         except Exception as exception:
             print(str(exception))
 
-    def getAllConstants(self):
-        print("-- Get all constants from database")
 
-    def addConstant(self, key, value):
-        print("-- Insert constant into database")
+    def getConstant(self, key):
+        """
+        Get constant value by his key
+        """
+        try:
+            print("-- Get key: " + str(key))
+            self.cursor.execute("SELECT key,value FROM constants WHERE key = ?", (key,))
+            results = self.cursor.fetchall()
+            if len(results) > 0:
+                if results[0][0] == key:
+                    print(results[0][1])
+                    return results[0][1]
+        except Exception as exception:
+            print("-- Exception: " + str(exception))
+
 
     def updateConstant(self, key, value):
+        """
+        Update constant into database
+        """
         print("-- Update constant into database")
 
-    def deleteConstant(self, key):
-        print("-- Delete constant into database")
 
     def initializeDatabase(self):
-        print("-- Database initialization")
-        self.sqliteConnection.execute("CREATE TABLE constants(key TEXT, value TEXT)")
-        # TODO: Insert keys with empty values
-        self.sqliteConnection.commit()
+        """
+        Create table and add constants keys (VIEW_ID, TELEGRAM_GROUP_ID, TELEGRAM_BOT_TOKEN)
+        """
+        try:
+            print("-- Database initialization")
+            result = self.cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='constants';")
+            if result.fetchone()[0] == 0:
+                print("-- Create table constants")
+                self.cursor.execute("CREATE TABLE constants(key TEXT NOT NULL UNIQUE, value TEXT NOT NULL);")
+
+            results = self.cursor.execute("SELECT key FROM constants WHERE key = 'VIEW_ID';")
+            if results.fetchone() == None:
+                print("-- Insert key VIEW_ID")
+                self.cursor.execute("INSERT INTO constants(key, value) VALUES ('VIEW_ID', '');")
+
+            results = self.cursor.execute("SELECT key FROM constants WHERE key = 'TELEGRAM_GROUP_ID';")
+            if results.fetchone() == None:
+                print("-- Insert key TELEGRAM_GROUP_ID")
+                self.cursor.execute("INSERT INTO constants(key, value) VALUES ('TELEGRAM_GROUP_ID', '');")
+
+            results = self.cursor.execute("SELECT key FROM constants WHERE key = 'TELEGRAM_BOT_TOKEN';")
+            if results.fetchone() == None:
+                print("-- Insert key TELEGRAM_BOT_TOKEN")
+                self.cursor.execute("INSERT INTO constants(key, value) VALUES ('TELEGRAM_BOT_TOKEN', '');")
+
+            self.sqliteConnection.commit()
+        except Exception as exception:
+            print("-- Exception: " + str(exception))
+
+
+    def resetDatabase(self):
+        """
+        Reset database, delete database file
+        """
+        try:
+            print("-- Reset database")
+            if self.sqliteConnection != None:
+                print("-- Close SQLite connection")
+                self.sqliteConnection.close()
+                
+            print("-- Drop database file")
+            os.remove("embeded_conception.db")
+        except Exception as exception:
+            print("Exception: " + str(exception))
+
 
 constantDao = ConstantDao()
--- ui.py
+++ ui.py
@@ -1,13 +1,14 @@
-print("--===== EMBEDED CONCEPTION =====--")
+class UI:
 
-# If constant are empty
-print("-- Please enter Google Analytics informations")
-view_id = input("Please enter Google Analytics VIEW_ID: ")
+    def setViewId(self):
+        # If constant are empty
+        print("-- Please enter Google Analytics informations")
+        return input("Please enter Google Analytics VIEW_ID: ")
 
-print("\n-- Please enter Telegram bot informations")
-telegram_group_id = input("Please enter Google Analytics TELEGRAM_GROUP_ID: ")
+    def setTelegramGroupId(self):
+        print("\n-- Please enter Telegram bot informations")
+        return input("Please enter Google Analytics TELEGRAM_GROUP_ID: ")
 
-print("\n-- Please enter Telegram bot informations")
-telegram_bot_token = input("Please enter Google Analytics TELEGRAM_BOT_TOKEN: ")
-
-# print("You entered: " + str(telegram_group_id))
+    def setTelegramBotToken(self):
+        print("\n-- Please enter Telegram bot informations")
+        return input("Please enter Google Analytics TELEGRAM_BOT_TOKEN: ")
